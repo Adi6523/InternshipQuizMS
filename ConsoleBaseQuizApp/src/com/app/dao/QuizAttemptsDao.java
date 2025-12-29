c868ea8aefd1fc85b40d0cdd5befcec207fe4a7f
Piyush Patil <>
2025-12-29 11:46:52 +0530

    Quiz_Attempt dao changed

--- ConsoleBaseQuizApp/src/com/app/dao/QuizAttemptsDao.java
+++ ConsoleBaseQuizApp/src/com/app/dao/QuizAttemptsDao.java
@@ -2,8 +2,6 @@ package com.app.dao;
 
 import com.app.util.DbUtil;
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
 
 public class QuizAttemptsDao implements AutoCloseable {
 
@@ -24,29 +22,6 @@ public class QuizAttemptsDao implements AutoCloseable {
         }
     }
     
-    
-    public List<String> getQuizScores(int quizId) throws SQLException {
-        List<String> list = new ArrayList<>();
-        String sql = """
-            SELECT u.name, qa.final_score, qa.total_questions
-				FROM quiz_attempts qa
-				JOIN users u ON qa.student_id = u.user_id
-				WHERE qa.quiz_id = ?
-
-        """;
-
-        try (PreparedStatement stmt = con.prepareStatement(sql)) {
-            stmt.setInt(1, quizId);
-            ResultSet rs = stmt.executeQuery();
-            while (rs.next()) {
-                list.add("Student: " + rs.getString("name") +
-                         " | Score: " + rs.getInt("final_score") +
-                         "/" + rs.getInt("total_questions"));
-            }
-        }
-        return list;
-    }
-    
     public void saveAttempt(int studentId, int quizId, int score, int total) throws SQLException {
 
         String sql = "INSERT INTO quiz_attempts (student_id, quiz_id, final_score, total_questions) VALUES (?,?,?,?)";
@@ -59,7 +34,6 @@ public class QuizAttemptsDao implements AutoCloseable {
         }
     }
 
-    
     public List<String> getQuizScores(int quizId) throws SQLException {
         List<String> list = new ArrayList<>();
         String sql = """
@@ -81,7 +55,7 @@ public class QuizAttemptsDao implements AutoCloseable {
         }
         return list;
     }
-    
+
     @Override
     public void close() throws SQLException {
         if (con != null)
